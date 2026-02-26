aa0fc2d114607d1864293f4bfee9fca6936e93b2
higashitaniyume <higashitaniyume@gmail.com>
2026-02-26 21:21:00 +0800

    b站下载视频

---
+++ Valency.QQBot.Hydrogen/Services/BBDownService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valency.QQBot.Hydrogen.Services
+{
+	internal class BBDownService
+	{
+		public BBDownService() { }
+	}
+}
